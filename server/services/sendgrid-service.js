3273b9e3a0fcfc4dc25ae066a4b33ba9752aed07
Gonzalo Flores <gonzaflores358@gmail.com>
2021-04-10 11:56:51 -0300

    ahora el mail se envia desde el from configurado en .env

--- server/services/sendgrid-service.js
+++ server/services/sendgrid-service.js
@@ -3,7 +3,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const send = async (msg) =>
   await sgMail.send({
-    to: msg.from,
+    to: process.env.SENDGRID_FROM,
     from: process.env.SENDGRID_FROM,
     subject: msg.subject,
     text: `De: ${msg.from}. Nombre: ${msg.name}. ${msg.text}`,
